2d50883411f070c7e2eb4097b18430c7d041dcf6
Brandon Fonnemann <branjosef@gmail.com>
2020-10-08 08:51:34 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,52 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/' do   #DONE_WORKS
+  redirect to "/articles"
   end
+
+  ##Index action  #DONE_WORKS
+  get '/articles' do 
+  @articles = Article.all 
+  erb :index
+  end
+
+
+  ##Create actions  #DONE WORKS
+  get '/articles/new' do 
+  erb :new
+  end
+  
+  post '/articles' do #DONE WORKS
+   @article = Article.create(params)
+   redirect to "/articles/#{@article.id}"
+  end
+
+   ##Read action - single article  ##DONE WORKS
+   get '/articles/:id' do
+   @article = Article.find_by_id(params[:id])
+   erb :show
+   end
+
+   ##EDIT actions  ##DONE WORKS
+   get '/articles/:id/edit' do 
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+   end
+
+   patch '/articles/:id' do   #DONE WORKS
+    #binding.pry
+    @article=Article.find_by_id(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    redirect to "/articles/#{@article.id}"
+   end
+
+    ##DELETE ActiON
+   delete '/articles/:id' do 
+    Article.destroy(params[:id])
+     redirect to '/articles'
+    end
+
 end
